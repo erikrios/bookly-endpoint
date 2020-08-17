04646dbecde340595093032d5cfbb02b0d8d1105
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-17 19:19:30 +0700

    Add Javascript Object

--- index.js
+++ index.js
@@ -9,34 +9,59 @@ const app = express();
 app.use(express.json());
 
 const books = [
-    {
+    {   
+        id: 1,
         title: "Coders at Work : Reflections on the Craft of Programming",
-        author: ["Peter Seibel"],
+        author: [
+            "Peter Seibel"
+        ],
         publisher: "APPRESS"
     },
     {
+        id: 2,
         title: "Head First: Design Patterns",
-        author: ["Eric Freeman", "Elisabeth Robson", "Bert Bates", "Kathy Sierra"],
+        author: [
+            "Eric Freeman",
+            "Elisabeth Robson",
+            "Bert Bates",
+            "Kathy Sierra"
+        ],
         publisher: "O'REILLY"
     },
     {
+        id: 3,
         title: "Head First HTML and CSS",
-        author: ["Elisabeth Robson", "Eric Freeman"],
+        author: [
+            "Elisabeth Robson",
+            "Eric Freeman"
+        ],
         publisher: "O'REILLY"
     },
     {
+        id: 4,
         title: "Head First Java",
-        author: ["Kathy Sierra", "Bert Bates"],
+        author: [
+            "Kathy Sierra",
+            "Bert Bates"
+        ],
         publisher: "O'REILLY"
     },
     {
+        id: 5,
         title: "MONSTER ARDUINO : Panduan Praktis Belajar Arduino untuk Pemula",
-        author: ["Hari Santoso"],
+        author: [
+            "Hari Santoso"
+        ],
         publisher: "Elang Sakti"
     },
     {
+        id: 6,
         title: "Head First Object-Oriented Analysis & Design",
-        author: ["Brett D. McLaughlin", "Gary Pollice", "David West"],
+        author: [
+            "Brett D. McLaughlin",
+            "Gary Pollice",
+            "David West"
+        ],
         publisher: "O'REILLY"
     }
 ]
@@ -46,6 +71,12 @@ app.get('/', (req, res) => {
     res.contentType('application/json');
     res.send(JSON.stringify({ message: "Hello, World!" }));
 });
+
+// Add GET HTTP Method to "/api/books" endpoint
+app.get('/api/books', (req, res) => {
+    res.contentType('application/json');
+    res.send(JSON.stringify(books));
+})
 
 // Create PORT
 const port = process.env.PORT || 3000;
