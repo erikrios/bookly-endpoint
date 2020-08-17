19217c3bffe0751ff59be0112f0e608eb6f2e23d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-17 19:13:19 +0700

    Add Javascript Object

--- index.js
+++ index.js
@@ -8,6 +8,39 @@ const app = express();
 // Add JSON middleware
 app.use(express.json());
 
+const books = [
+    {
+        title: "Coders at Work : Reflections on the Craft of Programming",
+        author: ["Peter Seibel"],
+        publisher: "APPRESS"
+    },
+    {
+        title: "Head First: Design Patterns",
+        author: ["Eric Freeman", "Elisabeth Robson", "Bert Bates", "Kathy Sierra"],
+        publisher: "O'REILLY"
+    },
+    {
+        title: "Head First HTML and CSS",
+        author: ["Elisabeth Robson", "Eric Freeman"],
+        publisher: "O'REILLY"
+    },
+    {
+        title: "Head First Java",
+        author: ["Kathy Sierra", "Bert Bates"],
+        publisher: "O'REILLY"
+    },
+    {
+        title: "MONSTER ARDUINO : Panduan Praktis Belajar Arduino untuk Pemula",
+        author: ["Hari Santoso"],
+        publisher: "Elang Sakti"
+    },
+    {
+        title: "Head First Object-Oriented Analysis & Design",
+        author: ["Brett D. McLaughlin", "Gary Pollice", "David West"],
+        publisher: "O'REILLY"
+    }
+]
+
 // Add GET HTTP Method to "/" endpoint
 app.get('/', (req, res) => {
     res.contentType('application/json');
