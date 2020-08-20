8ef8eb1527eb43b8a2f9a2fd7748b756aeefd585
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-20 11:33:37 +0700

    Create book schema

--- routes/books.js
+++ routes/books.js
@@ -66,6 +66,39 @@ mongoose.connect('mongodb://localhost/library', { useNewUrlParser: true, useUnif
     .then(() => console.log('Connected to MongoDB...'))
     .catch(error => console.log('Could not connect to MongoDB', error));
 
+// Create book schema
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255
+    },
+    author: {
+        type: Array,
+        validate: {
+            isAsync: true,
+            validator: function (v, callback) {
+                setTimeout(() => {
+                    const value = v && v.length > 0;
+                    callback(value);
+                }, 1000);
+            },
+            message: 'A book should have one author.'
+        }
+    },
+    publisher: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 255
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
 // Add GET HTTP Method to "/api/books" endpoint
 router.get('/', (req, res) => {
     res.contentType('application/json');
