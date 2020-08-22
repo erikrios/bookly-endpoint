69aaef86d6d8dbb0586e3c69681ae213ac2d7ba7
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-22 11:03:58 +0700

    Refactor the code

---
+++ models/book.js
@@ -0,0 +1,43 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const { authorSchema } = require('./author');
+
+// Create book schema
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255
+    },
+    authors: {
+        type: [authorSchema],
+        required: true
+    },
+    publisher: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Create model class
+const Book = mongoose.model('Book', bookSchema);
+
+function validateBook(book) {
+    const schema = {
+        title: Joi.string().min(5).max(100).required(),
+        authorId: Joi.string().length(24).required(),
+        publisher: Joi.string().min(5).max(100).required()
+    }
+}
+
+module.exports = {
+    Book: Book,
+    validateBook: validateBook
+}
