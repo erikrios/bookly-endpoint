a53d5a914acad542bd7d86a4035dd1c32753af0e
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-22 20:09:05 +0700

    Create books.js

---
+++ routes/books.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const { Book, validate } = require('../models/book');
+const { Author } = require('../models/author');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const books = await Book.find().sort('title');
+    res.send(books);
+});
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    const book = await Book.findById(id);
+
+    if (!book) return res.status(404).send('The author with the given ID was not found.');
+
+    res.send(book);
+});
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const authors = [];
+    req.body.authorsId.forEach(authorId => {
+        const author = await Author.findById(authorId).select('-__v');
+        if (!author) return res.status(404).send('The author with the given ID was not found.');
+        authors.push(author);
+    });
+
+    let book = new Book({
+        title: req.body.title,
+        authors: authors,
+        publisher: req.body.publisher
+    });
+
+    book = await book.save();
+    return res.send(book);
+});
+
+router.put('/:id', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const authors = [];
+    req.body.authorsId.forEach(authorId => {
+        const author = await Author.findById(authorId).select('-__v');
+        if (!author) return res.status(404).send('The author with the given ID was not found.');
+        authors.push(author);
+    });
+
+    const book = await Book.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        authors: authors,
+        publisher: req.body.publisher
+    }, { new: true });
+
+    if (!book) return res.status(404).send('The book with the given ID was not found.');
+
+    res.send(book);
+});
+
+router.delete('/:id', async (req, res) => {
+    const book = await Book.findByIdAndDelete(req.params.id);
+
+    if (!book) return res.status(404).send('The book with the given ID was not found.');
+
+    res.send(book);
+});
+
+module.exports = router;
