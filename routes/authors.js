88ffceb2e3a333fe926949eee6f67d4e674a5eb9
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-22 19:13:58 +0700

    Clean the code

--- routes/authors.js
+++ routes/authors.js
@@ -34,7 +34,7 @@ router.put('/:id', async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const author = Author.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true });
+    const author = await Author.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true });
 
     if (!author) return res.status(404).send('The author with the given ID was not found.');
     res.send(author);
