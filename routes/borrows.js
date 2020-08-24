63ed5ffeac8574ce141362a0c260e10e117df8df
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-24 17:33:05 +0700

    Create borrows.js

---
+++ routes/borrows.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const { Borrow, validate } = require('../models/borrow');
+const { Member } = require('../models/member');
+const { Book } = require('../models/book');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const borrows = await Borrow.find().sort('-dateOut');
+    res.send(borrows);
+});
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0]).message;
+
+    const member = Member.findById(req.body.memberId);
+    if (!member) return res.status(400).send('Invalid member.');
+
+    const book = Book.findById(req.body.bookId);
+    if (!book) return res.status(400).send('Invalid book.');
+
+    if (book.numberInStock === 0) return res.status(400).send('Book not in stock.');
+
+    let borrow = new Borrow({
+        member: {
+            _id: member._id,
+            name: member.name
+        },
+        book: {
+            _id: (await book)._id,
+            title: book.title
+        }
+    });
+
+    borrow = await borrow.save();
+
+    book.numberInStock--;
+    book.save();
+
+    res.send(borrow);
+});
+
+router.get('/:id', async (req, res) => {
+    const borrow = Borrow.findById(req.params.id);
+
+    if (!borrow) return res.status(404).send('The borrow with the given ID was not found.');
+
+    res.send(borrow);
+});
+
+module.exports = router;
