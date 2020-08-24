f61aa07680784a6fc09c2e5e708589c04ce7e5ba
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-24 20:07:06 +0700

    Create users.js

---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const { User, validate } = require('../models/user');
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(404).send(error.details[0].message);
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    await user.save();
+
+    res.send(user);
+});
+
+module.exports = router;
