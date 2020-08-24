b919c3c0e3942e3245b2b45ca52c721806d8ea71
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-24 20:09:27 +0700

    Fixing the status code

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
     const { error } = validate(req.body);
-    if (error) return res.status(404).send(error.details[0].message);
+    if (error) return res.status(400).send(error.details[0].message);
 
     const user = new User({
         name: req.body.name,
