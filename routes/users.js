565228d63d696e64cd2b06ae316e6e6721314b99
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-24 20:32:57 +0700

    Import lodash modules

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const _ = require('lodash');
 const { User, validate } = require('../models/user');
 
 const router = express.Router();
