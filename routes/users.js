9fd0e17ffc307a9a2e149fa5d585ebec9ca4830e
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-06 20:24:59 +0700

    Getting the current user

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,14 @@ const express = require('express');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const { User, validate } = require('../models/user');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
+
+router.get('/me', auth, async (req, res) => {
+    const user = await User.findById(req.user._id).select('-password');
+    res.send(user);
+});
 
 router.post('/', async (req, res) => {
     const { error } = validate(req.body);
