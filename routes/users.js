64d4def24cee6fe14be46291a4b9799163e3fa54
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-06 19:18:03 +0700

    Add header response token

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const express = require('express');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { User, validate } = require('../models/user');
 
 const router = express.Router();
@@ -18,7 +20,8 @@ router.post('/', async (req, res) => {
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
 
-    res.send(_.pick(user, ['_id', 'name', 'email']));
+    const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
