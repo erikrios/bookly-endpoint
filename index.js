8020bd5608b2eb7ab4dc9e17c68255fd5d2748f9
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-06 18:10:21 +0700

    Import home, books, members, authors, borrows, users, and auth routes

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
-// Import home, books, members, authors, borrows, and users routes
+// Import home, books, members, authors, borrows, users, and auth routes
 const home = require('./routes/home');
 const books = require('./routes/books');
 const members = require('./routes/members');
 const authors = require('./routes/authors');
 const borrows = require('./routes/borrows');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 
 // Create a connection to MongoDB
 mongoose.connect('mongodb://localhost/library', { useNewUrlParser: true, useUnifiedTopology: true })
