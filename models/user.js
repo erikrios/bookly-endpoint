a976cdd34d9123e5653291b2821546ba7634de84
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-06 20:33:28 +0700

    Add new user property

--- models/user.js
+++ models/user.js
@@ -22,7 +22,8 @@ const userSchema = new mongoose.Schema({
         minlength: 5,
         maxlength: 1024,
         required: true
-    }
+    },
+    isAdmin: Boolean
 });
 
 userSchema.methods.generateAuthToken = function () {
