cca64b525d57077ceb8993276bc486f7d37f1aca
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-06 19:28:52 +0700

    Add a method to generate auth token

--- models/user.js
+++ models/user.js
@@ -23,6 +23,11 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+userSchema.method.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+    return token;
+}
+
 const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
