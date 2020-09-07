6e0eae97e3ab33e0d8fb786e44673b01ebb5f56b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-07 12:33:05 +0700

    Handling uncaught exception

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ mongoose.connect('mongodb://localhost/library', { useNewUrlParser: true, useUnif
 // Create the instance of express
 const app = express();
 
+process.on('uncaughtException', (ex) => {
+    console.log('WE GOT AN UNCAUGHT EXCEPTION');
+    winston.error(ex.message, ex);
+});
+
 winston.add(winston.transports.File, { filename: 'logfile.log' });
 winston.add(winston.transports.MongoDB, {
     db: 'mongodb://localhost/library',
