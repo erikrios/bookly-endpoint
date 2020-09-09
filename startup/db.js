7a835dfc94b297d1ea2beb8c0b1ee2d934bca3d2
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-09 12:14:50 +0700

    Clean the code

--- startup/db.js
+++ startup/db.js
@@ -5,6 +5,7 @@ const config = require('config');
 module.exports = function () {
     // Create a connection to MongoDB
     const db = config.get('db');
-    mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+
+    mongoose.connect('bookly_db=mongodb+srv://bookly-user:A#T2Ew8b65@clusterbookly.rturt.mongodb.net/library?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => winston.info('Connected to MongoDB...'));
 }
