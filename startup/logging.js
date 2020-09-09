e082173c493802237178ba7159eec8b975533e34
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-09 13:29:00 +0700

    Clean the code

--- startup/logging.js
+++ startup/logging.js
@@ -14,7 +14,7 @@ module.exports = function () {
 
     winston.add(winston.transports.File, { filename: 'logfile.log' });
     // winston.add(winston.transports.MongoDB, {
-    //     db: 'mongodb+srv://erikriosetiawan:Erik1997@clusterbookly.rturt.mongodb.net/library?retryWrites=true&w=majority',
+    //     db: '',
     //     level: 'info'
     // });
 }
