03f66663c3b7d0b056b85cdf0c57a9484e734236
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-09 13:04:26 +0700

    Clean the code

--- startup/db.js
+++ startup/db.js
@@ -7,5 +7,6 @@ module.exports = function () {
     const db = config.get('db');
 
     mongoose.connect('mongodb+srv://erikriosetiawan:Erik1997@clusterbookly.rturt.mongodb.net/library?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => winston.info('Connected to MongoDB...'));
+        .then(() => winston.info('Connected to MongoDB...'))
+        .catch(ex => { console.log(ex) });
 }
